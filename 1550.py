5881ac0c56e0cb7523cba8e755ff82e63c9e8619
yonginggg <843852754@qq.com>
2024-03-05 16:52:28 +0800

    2024年3月5日

---
+++ 1550.py
@@ -0,0 +1,14 @@
+n,m,k = map(int, input().split())
+A = []
+B = []
+C = [[0]*k for i in range(n)]
+for i in range(n): A.append(list(map(int, input().split())))
+for i in range(m): B.append(list(map(int, input().split())))
+for i in range(n):
+    for j in range(m):
+        for l in range(k):
+            C[i][l] +=A[i][j]*B[j][l]
+for i in range(n):
+    for j in range(k):
+        print(C[i][j], end = ' ')
+    print()
