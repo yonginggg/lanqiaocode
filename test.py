a5fb6c55929341d371e1d3baa3ff6c7511ed45f3
yonginggg <843852754@qq.com>
2024-04-05 20:03:44 +0800

    2024年4月5日

--- test.py
+++ test.py
@@ -1,12 +1,30 @@
-import math
+# def dfs(s,t):
+#     if s == 3:                
+#         print(a[0:3])   
+#     else: 
+#         for i in range(s,t+1):
+#            a[s],a[i] = a[i],a[s]   #交换
+#            dfs(s+1,t)
+#            a[i],a[s] = a[s],a[i]   #恢复
 
-n = int(input())
-c = int(math.log(n,2))+1   #一共有c层
+# a = [1,2,3,4,5,6,7,8,9] 
+# n = 4
+# dfs(0,n-1)                 #前n个数的全排列
 
-a = [0]+list(map(int, input().split())) #a[1]~a[n]
-s = [0] * (c+1)   #记录每层的和，s[1]~s[c]
+def dfs(s,t):
+    if s == 3:             #递归结束，输出一个全排列    
+        print(b[0:3]) 
+    else: 
+        for i in range(t):
+            if vis[i] == False:
+               vis[i] = True
+               b[s]=a[i]
+               dfs(s+1,t)
+               vis[i] = False           
 
-for i in range(1,c+1):            #第1层到第c层
-    s[i] = sum(a[2**(i-1): 2**i-1 +1])#注意切片范围
+a = [1,2,3,4,5,6,7,8,9]
+b = [0]*10           #记录生成的一个全排列
+vis = [False]*10     #记录第i个数是否用过
+n = 4
+dfs(0,n)            #前n个数的全排列
 
-print(s.index(max(s)))
